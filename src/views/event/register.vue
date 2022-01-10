c5123ab543bfc3bb18426585cb41f423a44c0c7d
Angie <angiecristinagonzalessara@gmail.com>
2022-01-09 22:46:23 -0500

    add errors views

--- src/views/event/register.vue
+++ src/views/event/register.vue
@@ -1,9 +1,17 @@
 /* eslint-disable prettier/prettier */
 <template>
   <p>Register for the event here</p>
+  <button @click="register">Register me</button>
 </template>
 <script>
 export default {
   props: ['event'],
+  methods: {
+    register() {
+      this.$router.push({
+        name: 'EventDetails',
+      });
+    },
+  },
 };
 </script>
