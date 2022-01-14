6002ef5938b8c0cb75972dfa2374e00462574023
Angie <angiecristinagonzalessara@gmail.com>
2022-01-13 19:56:33 -0500

    add flash message using GStore reactive vue-3

--- src/views/event/register.vue
+++ src/views/event/register.vue
@@ -6,10 +6,18 @@
 <script>
 export default {
   props: ['event'],
+  inject: ['GStore'],
   methods: {
     register() {
+      this.GStore.flashMessage =
+        'You are successfully registered for' + this.event.title;
+      setTimeout(() => {
+        //after 3 second delete the message
+        this.GStore.flashMessage = '';
+      }, 3000);
       this.$router.push({
         name: 'EventDetails',
+        params: { id: this.event.id },
       });
     },
   },
